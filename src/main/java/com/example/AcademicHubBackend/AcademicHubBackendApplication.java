697d3a39ae8d634b78c565084490e8d7d2f66ce7
AnanyaNagar <ananyanagar32@gmail.com>
2022-10-12 14:19:28 +0530

    Creating basic boilerplate code

---
+++ src/main/java/com/example/AcademicHubBackend/AcademicHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.AcademicHubBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicHubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AcademicHubBackend/AcademicHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AcademicHubBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
