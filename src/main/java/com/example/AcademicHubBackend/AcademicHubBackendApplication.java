4b28b7ad8f43f7979fede6f5fe1405197a79cf1f
AnanyaNagar <ananyanagar32@gmail.com>
2022-11-09 17:50:34 +0530

    Minor changes

--- src/main/java/com/example/AcademicHubBackend/AcademicHubBackendApplication.java
+++ src/main/java/com/example/AcademicHubBackend/AcademicHubBackendApplication.java
@@ -3,7 +3,6 @@ package com.example.AcademicHubBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
